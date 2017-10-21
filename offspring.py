cac7b8025981d18e700d9715c467bd3139181d71
Doug Swisher <big.swish@gmail.com>
2017-10-21 11:33:23 -0500

    Update offspring.py to actually check genotypes

--- offspring.py
+++ offspring.py
@@ -10,13 +10,49 @@ def parse(fp):
         return {}
     bits = line.split('\t')
     id = bits[0]
-    # chromosome = bits[1]
-    # position = bits[2]
+    chromosome = bits[1]
+    position = bits[2]
     genotype = bits[3]
-    return {"id": id, "genotype": genotype}
+    return {
+            "id": id,
+            "genotype": genotype,
+            "chromosome": chromosome,
+            "position": position}
+
+
+def print_mismatch(mrec, frec, crec):
+    fmt = "MISMATCH! id: {:<12}  chr: {:>2} pos: {:>10} mg: {:2} fg: {:2} cg: {:2}"     # noqa
+    print (fmt.format(mrec["id"],
+                      mrec["chromosome"],
+                      mrec["position"],
+                      mrec["genotype"],
+                      frec["genotype"],
+                      crec["genotype"]))
+
+
+def compare_genotypes(mrec, frec, crec):
+    mg = mrec["genotype"]
+    fg = frec["genotype"]
+    cg = crec["genotype"]
+
+    # Ignore missing data
+    if mg == "--" or fg == "--" or cg == "--":
+        return True
+    if len(mg) < 2 or len(fg) < 2 or len(cg) < 2:
+        return True
+
+    # Check 'em
+    if cg[0] in mg and cg[1] in fg:
+        return True
+    if cg[0] in fg and cg[1] in mg:
+        return True
+
+    # No match?
+    return False
 
 
 def main(mother, father, child):
+    mismatches = 0
     with open(mother, "r") as mp:
         with open(father, "r") as fp:
             with open(child, "r") as cp:
@@ -26,7 +62,10 @@ def main(mother, father, child):
                     frec = parse(fp)
                     crec = parse(cp)
                     if mrec is None or frec is None or crec is None:
-                        print "END!"
+                        if mrec is not None or frec is not None or crec is not None:    # noqa
+                            print "END, but INCOMPLETE!"
+                        else:
+                            print "END!"
                         break
                     if len(mrec) == 0 or len(frec) == 0 or len(crec) == 0:
                         continue
@@ -37,6 +76,12 @@ def main(mother, father, child):
                     if num > 1000000:
                         print "LIMIT!"
                         break
+
+                    if not compare_genotypes(mrec, frec, crec):
+                        mismatches += 1
+                        if mismatches < 200:
+                            print_mismatch(mrec, frec, crec)
+    print "Total mismatches:", mismatches
 
 
 if len(sys.argv) < 4:
